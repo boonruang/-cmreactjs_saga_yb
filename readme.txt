8c7fb34bc02705f93e4bd8cccd1b5535043723ec
boonruang <scubatoy@gmail.com>
2022-02-13 19:45:34 +0700

    ep3 add redux-devtools-extension

--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 npm i -g yarn
 yarn add redux react-redux redux-thunk redux-saga
+
+npm install --save redux-devtools-extension
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,4 @@
+import counterReducer from './coutner.reducer'
+import { combineReducers } from 'redux'
+
+export default combineReducers({ counterReducer })
